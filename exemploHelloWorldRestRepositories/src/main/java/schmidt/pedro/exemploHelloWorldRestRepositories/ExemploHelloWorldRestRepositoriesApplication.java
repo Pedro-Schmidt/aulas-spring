228377afe3f3b94ae410ba6215c556a83a9101cc
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-10 18:19:45 -0300

    Hello World com rest repositories

--- exemploHelloWorldRestRepositories/src/main/java/schmidt/pedro/exemploHelloWorldRestRepositories/ExemploHelloWorldRestRepositoriesApplication.java
+++ exemploHelloWorldRestRepositories/src/main/java/schmidt/pedro/exemploHelloWorldRestRepositories/ExemploHelloWorldRestRepositoriesApplication.java
@@ -8,6 +8,7 @@ public class ExemploHelloWorldRestRepositoriesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExemploHelloWorldRestRepositoriesApplication.class, args);
+
 	}
 
 }
