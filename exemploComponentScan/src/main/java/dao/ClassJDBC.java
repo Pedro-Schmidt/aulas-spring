c5bbdc9d917cf77faed4a35478b1ebe9b3706cf6
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-12 10:06:01 -0300

    Exemplo ComponentScan

---
+++ exemploComponentScan/src/main/java/dao/ClassJDBC.java
@@ -0,0 +1,10 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassJDBC {
+    public ClassJDBC() {
+        System.out.println("Class JDBC");
+    }
+}
--- exemploComponentScan/src/main/java/schmidt/pedro/exemploComponentScan/ExemploComponentScanApplication.java
+++ exemploComponentScan/src/main/java/schmidt/pedro/exemploComponentScan/ExemploComponentScanApplication.java
@@ -1,13 +1,25 @@
 package schmidt.pedro.exemploComponentScan;
 
+import dao.ClassDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("dao")
 public class ExemploComponentScanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExemploComponentScanApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(ExemploComponentScanApplication.class, args);
+
+		ClassDAO objectDAO = applicationContext.getBean(ClassDAO.class);
+
+		System.out.println("ObjectDAO: " + objectDAO);
+
+		System.out.println("ObjectJDBC dentro do ObjectDAO: " + objectDAO.getClassJDBC());
+
 	}
 
 }
