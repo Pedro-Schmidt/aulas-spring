97e9d0913d97db4c65ead4e4246b80bd7673dd1c
Pedro Schmidt <schmidthu3@outlook.com>
2020-06-16 13:40:22 -0300

    First Commit

---
+++ exemploH2Database/src/test/java/schmidt/pedro/exemploH2Database/ExemploH2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package schmidt.pedro.exemploH2Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploH2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
